bdd990d41b8d96c6668883908eb16d3fb2e52b2e
Admin <Admin@ADMIN>
2025-12-06 06:35:33 +0700

    refactor(e_card): reorganize imports and improve code formatting
    Update import paths to use relative imports for better maintainability
    Clean up code formatting and remove unnecessary comments

--- lib/features/e_card/domain/usecases/create_new_card_use_case.dart
+++ lib/features/e_card/domain/usecases/create_new_card_use_case.dart
@@ -1,24 +1,27 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:online_wedding/core/error/failures.dart';
-import 'package:online_wedding/core/usecases/usecase.dart';
-import 'package:online_wedding/features/e_card/domain/entities/wedding_card_entity.dart';
-import 'package:online_wedding/features/e_card/domain/repositories/e_card_repository.dart';
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/wedding_card_entity.dart';
+import '../repositories/e_card_repository.dart';
 
-final createNewCardUseCaseProvider = Provider<CreateNewCardUseCase>((ref) {
-  // This will be updated later to use a real repository provider
+final createNewCardUseCaseProvider =
+    Provider<CreateNewCardUseCase>((ref) {
   final repository = ref.watch(eCardRepositoryProvider);
   return CreateNewCardUseCase(repository);
 });
 
-class CreateNewCardUseCase extends UseCase<WeddingCardEntity, CreateNewCardParams> {
+class CreateNewCardUseCase
+    extends UseCase<WeddingCardEntity, CreateNewCardParams> {
   final ECardRepository repository;
 
   CreateNewCardUseCase(this.repository);
 
   @override
-  Future<Either<Failure, WeddingCardEntity>> call(CreateNewCardParams params) async {
+  Future<Either<Failure, WeddingCardEntity>> call(
+    CreateNewCardParams params,
+  ) async {
     return await repository.createNewCard(params);
   }
 }
@@ -40,7 +43,6 @@ class CreateNewCardParams extends Equatable {
   List<Object?> get props => [templateId, coupleName, date, isPremium];
 }
 
-// This is a placeholder for the actual repository provider
 final eCardRepositoryProvider = Provider<ECardRepository>((ref) {
   throw UnimplementedError();
 });
